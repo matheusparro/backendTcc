2df8f7084ec6f55ffe3888a1998a5fdbd2967a09
Matheus Parro <mathparro@gmail.com>
2022-05-10 23:03:13 -0300

    Heroku

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
 import { app } from "./app";
 
-app.listen(process.env.PORT || 3333, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT);
